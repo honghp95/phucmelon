376d76d6d14fca279302b47014e881c9d25ff12b
HongHP <honghp95@gmail.com>
2020-03-25 14:23:41 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Anh Phúc bị nhiễm covid-19 nâng tổng số ca nhiễm lên 135. :D
         </p>
         <a
           className="App-link"
